bc26865f44f3fd02a8abc3be23b6ca2e296857cb
syu-kylin <shuwen.y68@gmail.com>
2025-09-12 12:54:40 +0000

    update rr_extraction

--- rr_extraction.py
+++ rr_extraction.py
@@ -1,3 +1,4 @@
+
 import os
 import math
 import copy
@@ -21,12 +22,8 @@ plt.style.use(['science', 'no-latex'])
 from Pan_Tomkin_enhanced import Pan_Tompkin_preprocessing as Pan_Tompkin_preprocessing_enhanced
 from Pan_Tomkin_enhanced import QRS_detect
 
-from Pan_Tompkin_stand import Pan_Tompkin_preprocessing as Pan_Tompkin_preprocessing_standard
-from Pan_Tompkin_stand import detect_peaks
-
 
 logger = logging.getLogger('rr_extraction')
-
 
 def main(edf_fn, select_chann_name):
     """
@@ -49,68 +46,47 @@ def main(edf_fn, select_chann_name):
     r_peaks_enhanced, rr_interval_corrected, time_stamp_corrected = hr.detect_r_peaks()
     rr_time_stamp_enhanced = time_arr_enhanced[r_peaks_enhanced]
     rr_intervals_enhanced = np.diff(rr_time_stamp_enhanced)
-
-    # standard Pan-Tompkins
-    logger.info("Starting standard Pan-Tompkins preprocessing...")
-    sfreq, ecg_arr_stand, time_arr_stand, b_pass_stand, _, _, ma_win_stand, chann_name, reverse_flag = Pan_Tompkin_preprocessing_standard(edf_fn, select_chann_name)
-    r_peaks_stand = detect_peaks(ecg_arr_stand, b_pass_stand, ma_win_stand, sfreq)
-
-    rr_time_stamp_stand = time_arr_stand[r_peaks_stand]
-    rr_intervals_stand = np.diff(rr_time_stamp_stand)
-
-    # Plotting rr intervals
-    logger.info("Plotting RR intervals...")
-    plot_rr(rr_time_stamp_stand, rr_intervals_stand, rr_time_stamp_enhanced, rr_intervals_enhanced, time_stamp_corrected, rr_interval_corrected)
+    logger.info(f"RR extraction done!")
+    plot_rr(rr_time_stamp_enhanced, rr_intervals_enhanced, time_stamp_corrected, rr_interval_corrected)
 
 
-
-
-def plot_rr(rr_time_stamp_stand, rr_interval_stand, rr_time_stamp_enhanced, rr_interval_enhanced, rr_time_stamp_corrected, rr_interval_corrected):
+def plot_rr(rr_time_stamp_enhanced, rr_interval_enhanced, rr_time_stamp_corrected, rr_interval_corrected):
     """
     Plot RR intervals for standard and enhanced Pan-Tompkins methods.
     """
-    mean_stand = np.mean(rr_interval_stand)
-    std_stand = np.std(rr_interval_stand)
     mean_enhanced = np.mean(rr_interval_enhanced)
     std_enhanced = np.std(rr_interval_enhanced)
     std_corrected = np.std(rr_interval_corrected)
 
     # Plotting
-    fig, axs = plt.subplots(nrows=3, ncols=1, figsize=(10,9), constrained_layout=True)
+    fig, axs = plt.subplots(nrows=2, ncols=1, figsize=(10,6), constrained_layout=True)
 
-    text0 = f"mean: {mean_stand:.4f}\nstd: {std_stand:.4f}"
-    axs[0].plot(rr_time_stamp_stand[1:], rr_interval_stand)
-    if min(rr_interval_stand)<0.3:
+    text0 = f"mean: {mean_enhanced:.4f}\nstd: {std_corrected:.4f}"
+    axs[0].plot(rr_time_stamp_corrected[1:], rr_interval_corrected)
+    if min(rr_interval_corrected) < 0.3:
         axs[0].axhline(0.2,color='r')
-    axs[0].set_title(f'rr intervals from standard Pan-Tompkin')
+    axs[0].set_title(f'corrected rr intervals from enhanced Pan-Tompkin')
     axs[0].text(0.9, 0.83, text0, transform=axs[0].transAxes)
     axs[0].set_xlabel('time(s)')
     axs[0].grid()
 
-    text1 = f"mean: {mean_enhanced:.4f}\nstd: {std_corrected:.4f}"
-    axs[1].plot(rr_time_stamp_corrected[1:], rr_interval_corrected)
-    if min(rr_interval_corrected) < 0.3:
+    text1 = f"mean: {mean_enhanced:.4f}\nstd: {std_enhanced:.4f}"
+    axs[1].plot(rr_time_stamp_enhanced[1:], rr_interval_enhanced)
+    if min(rr_interval_enhanced) < 0.3:
         axs[1].axhline(0.2,color='r')
-    axs[1].set_title(f'corrected rr intervals from enhanced Pan-Tompkin')
+    axs[1].set_title(f'rr intervals from enhanced Pan-Tompkin')
     axs[1].text(0.9, 0.83, text1, transform=axs[1].transAxes)
     axs[1].set_xlabel('time(s)')
     axs[1].grid()
-
-    text2 = f"mean: {mean_enhanced:.4f}\nstd: {std_enhanced:.4f}"
-    axs[2].plot(rr_time_stamp_enhanced[1:], rr_interval_enhanced)
-    if min(rr_interval_enhanced) < 0.3:
-        axs[2].axhline(0.2,color='r')
-    axs[2].set_title(f'rr intervals from enhanced Pan-Tompkin')
-    axs[2].text(0.9, 0.83, text2, transform=axs[2].transAxes)
-    axs[2].set_xlabel('time(s)')
-    axs[2].grid()
 
     # Save the figure
     save_dir = './output'
     if not os.path.exists(save_dir):
         os.makedirs(save_dir)
-    plt.savefig(f"{save_dir}/rr interval comparison.svg")
+    plt.savefig(f"{save_dir}/rr interval.svg")
+    logger.info(f"RR intervals plot saved to {save_dir}/rr interval.svg")
     plt.close()
+
 
 def channel_selection(file_id_raw, channel_df):
     '''
